a2a34bb208f3aefefb8b48fc990fd4ef8f830689
«Kristina» <kristinka007_92@mail.ru>
2023-04-06 11:54:52 +0300

    Задача 38

--- Phone_book.py
+++ Phone_book.py
@@ -34,21 +34,45 @@ def find_file():
                 print(line)
 
 
+# def change_file():
+#     find_info = input()
+#     new_info = input()
+#     with open(file_path,'r+') as f:
+#         for line in f:
+#             if find_info.casefold() in line.casefold():
+#                 if input("Да/Нет: ") == "Да":
+#                     lst = (line.strip()).split(' ')
+#                     print(lst)
+#                 else: continue
+
+
 def change_file():
-    find_info = input()
-    new_info = input()
-    with open(file_path,'r+') as f:
-        for line in f:
-            if find_info.casefold() in line.casefold():
-                if input("Да/Нет") == "Да":
-                    lst = (line.strip()).split(' ')
-                    print(lst)
-                else: continue
+    with open(file_path, 'r+', encoding="utf-8") as open_book:
+        seach_param = (input('Введите параметр для поиска: ' ).title())
+        with open (file_path, 'w', encoding='utf8') as open_book:
+            for line in seach_param:
+                if seach_param in line:
+                    print(line)
+                    add_f = (input('Введите фамилию: ' ).title())
+                    add_i = (input('Введите Имя: ' ).title())
+                    add_tel = (input('Введите телефон: ' ).title())
+                    new_line = add_f +' '+add_i +' '+ add_tel + '\n'
+                    line = line.replace(line, new_line)
+                open_book.writelines(line)
 
 
+def delete_file():
+    with open(file_path, 'r', encoding="utf-8") as f:
+        X = input('Введите Имя или Фамилию для удаления: ')
+        lines = f.readlines()
+        with open(file_path, 'w', encoding="utf-8") as f:
+            for line in lines:
+                if X in line:
+                    print("Строка удалена")
+                else:
+                    print(line)    
+                    f.write(line)
 
-# def main():
-#     find_file()
 
 file_path = r'telephon_list.txt'
 change_function()
